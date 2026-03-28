57737c7773931ed1c4809969384ccfdd5193b01d
Артем <artem.bond2017@mail.ru>
2026-03-29 00:35:50 +0300

    add autogenerate uuid fields

--- app/database/models.py
+++ app/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey, UUID
+import uuid
+
+from sqlalchemy import Column, String, DateTime, ForeignKey, UUID, text
 from sqlalchemy.sql import func
 from sqlalchemy.orm import declarative_base
 
@@ -7,32 +9,32 @@ Base = declarative_base()
 class Env(Base):
     __tablename__ = 'env'
     
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4, server_default=text("gen_random_uuid()"))
     name = Column(String(20), unique=True, nullable=False)
 
 
 class Domain(Base):
     __tablename__ = 'domain'
     
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True, server_default=text("gen_random_uuid()"))
     name = Column(String(20), unique=True, nullable=False)
 
 
 class Project(Base):
     __tablename__ = 'project'
     
-    id = Column(UUID(as_uuid=True), primary_key=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True, server_default=text("gen_random_uuid()"))
     name = Column(String(20), unique=True, nullable=False)
 
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
     
-    id = Column(UUID(as_uuid=True), primary_key=True, index=True)
+    id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4, server_default=text("gen_random_uuid()"))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     login = Column(String, index=True, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    project_id = Column(UUID(as_uuid=True), ForeignKey('project.id'), nullable=True)
+    project_id = Column(UUID(as_uuid=True), ForeignKey('project.id'))
     env_id = Column(UUID(as_uuid=True), ForeignKey('env.id'))
     domain_id = Column(UUID(as_uuid=True), ForeignKey('domain.id'))
     locktime = Column(DateTime(timezone=True), server_default=func.now())
