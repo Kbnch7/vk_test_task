a3803089e3b99a38176a07911f58d87b9833da3a
Артем <artem.bond2017@mail.ru>
2026-03-30 01:00:41 +0300

    add probes

--- app/config.py
+++ app/config.py
@@ -4,5 +4,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 PASSWORD_ENCRYPTION_KEY = os.getenv("PASSWORD_ENCRYPTION_KEY")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
 print(f"DEBUG: DATABASE_URL is {os.getenv('DATABASE_URL')}")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi_healthchecks.api.router import HealthcheckRouter, Probe
+from fastapi_healthchecks.checks.postgres import PostgreSqlCheck
 
 from .api.v1 import users_router
+from .config import DATABASE_URL, POSTGRES_USER, POSTGRES_PASSWORD, DATABASE_HOST, DATABASE_NAME, DATABASE_PORT
 
 
 app = FastAPI()
@@ -13,3 +16,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(users_router, prefix='/users')
+app.include_router(
+    HealthcheckRouter(
+        Probe(
+            name="readiness",
+            checks=[
+                PostgreSqlCheck(host=DATABASE_HOST, username=POSTGRES_USER, password=POSTGRES_PASSWORD, port=DATABASE_PORT, database=DATABASE_NAME),
+            ],
+        ),
+        Probe(
+            name="liveness",
+            checks=[]
+        ),
+    ),
+    prefix="/health",
+)
